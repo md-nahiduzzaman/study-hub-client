80592ee1b04ce355206647dd2df7d37e7f396b64
md-nahiduzzaman <md_nahiduzzaman@outlook.com>
2024-05-11 20:19:23 +0600

    assignment details page complete

--- src/pages/AssignmentDetails.jsx
+++ src/pages/AssignmentDetails.jsx
@@ -20,23 +20,26 @@ const AssignmentDetails = () => {
   return (
     <>
       <div>
-        <h1>view assignment details {_id}</h1>
-      </div>
-      <div>
-        <div>
+        <div className="container mx-auto px-2 min-h-[calc(100vh-168px)]">
           <section className="p-6">
             <div className="container grid gap-6 mx-auto lg:grid-cols-2 xl:grid-cols-5">
               <img
-                src="https://source.unsplash.com/random/480x360"
-                alt=""
-                className="object-cover w-full rounded-md xl:col-span-2 "
+                src={image}
+                alt="image"
+                className="object-cover w-full h-full rounded-md xl:col-span-2 "
               />
-              <div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-3">
-                <h1 className="text-5xl font-extrabold ">Titel</h1>
-                <p className="my-8">description</p>
+              <div className="w-full p-8 rounded-md   xl:col-span-3">
+                <h1 className="text-5xl font-extrabold ">{title}</h1>
+                <p className="my-8">{description}</p>
                 <div>
-                  <p>Mark: 40</p>
-                  <p>Dificaltiy: easy</p>
+                  <p>Marks: {marks}</p>
+                  <p>Difficulty Level: {difficultyLevel}</p>
+                  <p>Deadline: {new Date(deadline).toLocaleDateString()}</p>
+                </div>
+                <div className="mt-8 mb-8">
+                  <h1>Owner Details</h1>
+                  <p>Name: {owner?.name}</p>
+                  <p>Email: {owner?.email}</p>
                 </div>
                 <div>
                   <Link>
